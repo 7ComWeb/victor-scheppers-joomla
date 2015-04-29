39819f42bef1bff5acf85a3848dc86bc740b9160
Lars De Richter <lars@anansi.be>
2015-04-29 15:10:16 +0200

    initial commit, with start of a base style

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+
+<!doctype html>
+<html lang="<?php echo $this->language; ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <jdoc:include type="head" />
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700,400italic,700italic' rel='stylesheet'>
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/styles.css">
+    <title>De Broeders van O.L.V. van Scheppers</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header role="banner">
+            <img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/images/banner.png" alt="Honor Deo, labor mihi, utilitas proximo" class="banner-image">
+            <h1>Broeders van O.L.V. van Barmhartigheid</h1>
+            <nav role="navigation" class="top-nav">
+                <button class="hamburger">Menu</button>
+                <jdoc:include type="modules" name="top-menu" />
+            </nav>
+        </header>
+
+        <aside class="left">
+            <jdoc:include type="modules" name="left" />
+        </aside>
+
+        <main class="clearfix">
+            <jdoc:include type="modules" name="before" />
+            <jdoc:include type="component" />
+            <jdoc:include type="modules" name="after" />
+        </main>
+
+        <aside class="right">
+            <jdoc:include type="modules" name="right" />
+        </aside>
+    </div>
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script>
+    if (window.matchMedia("(max-width: 854px)").matches) {
+        $(".menu-items").toggle(0);
+    }
+    $(".hamburger").click(function() {
+        $(this).siblings(".menu-items").toggle(500);
+    });
+    </script>
+</body>
+
+</html>
