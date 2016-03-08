579956ca864c9872c00850f60ad16dbbac34f0a3
Lars De Richter <lars@anansi.be>
2016-03-08 15:33:37 +0100

    Bug-fixes

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ $config = JFactory::getConfig();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <![endif]-->
+    <!--[if IE]>
+        <script>document.createElement('main');</script>
+    <![endif]-->
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700,400italic,700italic' rel='stylesheet'>
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/styles.css">
     <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/jquery-rebox.css">
